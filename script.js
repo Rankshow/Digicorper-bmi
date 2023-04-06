a3afda999e5674e2f78927facbb16a9212ef2a5c
Rankshow <uwaomaobinna20@gmail.com>
2023-04-06 23:17:52 +0100

    added padding to text

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-/*const body = document.querySelector('.lastest')
-const session = document.createElement('div');
-body.append(session);
-session.textContent = 'create dove';
-console.log(session);
-session.style.backgroundColor = 'red';
-session.style.padding = '10px';
-session.style.color = '#FFF';
-session.style.fontSize = '25px';
-session.style.textAlign = 'CENTER';
-session.style.height = '50px';
-const linkGoogle = document.createElement('a');
-linkGoogle.textContent = 'Googe link';
-linkGoogle.setAttribute('href', 'https://www.google.com')
-linkGoogle.setAttribute('target', '_blank')*/
-
 
 // fuction to get the value;
 const submitButton = document.querySelector('.btn');
@@ -31,15 +15,15 @@ const calculateSum = () => {
     const height = Number(document.getElementById('height').value);
     const weight = Number(document.getElementById('weight').value);
     const error = document.getElementById('error');
+    
     // suming the two value
-    const sum = Math.floor(weight / height) ** 2 ;
-
-    if (sum > 2.5){
-       output.innerHTML = `
-       <h5> The sum of the two value ${height} (in cm) and ${weight} (in kg) is ${sum} (kgcm) <i>your bmi is very high</i> </h5>
-       `
-    }
-
+    const sum = Math.ceil(weight / height) * 2 ;
+    
+    if (sum > 24){
+        output.innerHTML = `
+        <h5> The sum of the two value ${height} (in cm) and ${weight} (in kg) is ${sum} (kgcm) <i>your bmi is very high</i> </h5>
+           `
+        }
     // Throw error in any of the input is underfine.
     if (!height || !weight){
          error.textContent = 'Please Enter the height or weight';
@@ -68,15 +52,7 @@ const calculateSum = () => {
     
 }
     
-    
-    // function checkInput() {
-        //     if (!height || !weight) {
-            //         error.textContent = 'Please enter your correct height';
-            //         error.style.backgroundColor = 'red';
-            //     } if(height == 70 && weight == 40 ) {
-                //         error.textContent = '';
-                //         error.style.backgroundColor = 'green';
-                //     }
+     //     }
                 // }
                 
                 
